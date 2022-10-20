f655ef62a0aba4585c0667c44e081ca9f3861596
flomflam <32689713+flomflam@users.noreply.github.com>
2022-10-20 14:32:36 +0000

    Adding units and syntax lesson

---
+++ Unit1/syntax.py
@@ -0,0 +1,52 @@
+#Print "Hello World"
+
+
+#print the seasons on separate lines
+
+
+#leave a comment about your code
+#this is my comment!
+
+
+
+#Exercises:
+#1. Print the phrase “Hello, [your name]”
+#2. Print the words “red”, “white”, and “blue”, all on their own lines
+#3. Print those same words, but put 2 lines between each word
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Print "Hello World"
+#print("Hello World")
+
+#print the seasons on separate lines
+#print("spring\nsummer\nfall\nwinter")
+
+#leave a comment about your code
+#this is my comment!
+
+#print(2*"\n")
+
