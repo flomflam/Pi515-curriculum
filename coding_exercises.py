df997ba2b0e62fdc425862852d082bf088bd7f9f
Peter Hartmann <36562108+pet-erhartmann@users.noreply.github.com>
2022-10-27 15:33:47 -0500

    Create coding_exercises.py

---
+++ coding_exercises.py
@@ -0,0 +1,8 @@
+# Exercise 1A: Create a string made of the first, middle and last character
+# Write a program to create a new string made of an input string’s first, middle, and last character.
+
+# Given:
+# str1 = "James"
+
+# Expected Output:
+# "Jms"
